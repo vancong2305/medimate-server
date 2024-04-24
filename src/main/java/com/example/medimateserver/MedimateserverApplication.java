773747d405e2965cdac88fd49447958b096ed843
vancong2305 <truongvancong230502@gmail.com>
2024-04-24 11:09:15 +0700

    Update MedimateserverApplication.java

--- src/main/java/com/example/medimateserver/MedimateserverApplication.java
+++ src/main/java/com/example/medimateserver/MedimateserverApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Arrays;
-
 @SpringBootApplication
 public class
 MedimateserverApplication {
@@ -14,6 +13,8 @@ MedimateserverApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(MedimateserverApplication.class, args);
+
+
 	}
 
 }
